06473ff72d32d332ac13b1c30ad603934679f027
Hidayet Gencer <hidayet@gmail.com>
2020-03-22 14:49:39 +0100

    mockup data

--- Mobile/data.js
+++ Mobile/data.js
@@ -1,47 +1,124 @@
-const order = [
+const data = [
   {
-    firstname: "<firstname>",
+    id:1,
+    firstname: "Hido",
     lastname: "<lastname>",
     streetAndNumber: "<streetAndNumber>",
     zipCode: "zipCode",
     city: "city",
     lat: 0,
-    long: 0
+    long: 0,
+    ok:true
   },
   {
+    id:3,
     firstname: "<firstname>",
     lastname: "<lastname>",
     streetAndNumber: "<streetAndNumber>",
     zipCode: "zipCode",
     city: "city",
     lat: 0,
-    long: 0
+    long: 0,
+    ok:false
   },
   {
+    id:4,
     firstname: "<firstname>",
     lastname: "<lastname>",
     streetAndNumber: "<streetAndNumber>",
     zipCode: "zipCode",
     city: "city",
     lat: 0,
-    long: 0
+    long: 0,
+    ok:true
   },
   {
+    id:5,
     firstname: "<firstname>",
     lastname: "<lastname>",
     streetAndNumber: "<streetAndNumber>",
     zipCode: "zipCode",
     city: "city",
     lat: 0,
-    long: 0
+    long: 0,
+    ok:false
   },
   {
+    id:6,
     firstname: "<firstname>",
     lastname: "<lastname>",
     streetAndNumber: "<streetAndNumber>",
     zipCode: "zipCode",
     city: "city",
     lat: 0,
-    long: 0
-  }
+    long: 0,
+    ok:false
+  },
+    {
+      id:1,
+      firstname: "Hido",
+      lastname: "<lastname>",
+      streetAndNumber: "<streetAndNumber>",
+      zipCode: "zipCode",
+      city: "city",
+      lat: 0,
+      long: 0,
+      ok:true
+    },
+    {
+      id:3,
+      firstname: "<firstname>",
+      lastname: "<lastname>",
+      streetAndNumber: "<streetAndNumber>",
+      zipCode: "zipCode",
+      city: "city",
+      lat: 0,
+      long: 0,
+      ok:false
+    },
+    {
+      id:4,
+      firstname: "<firstname>",
+      lastname: "<lastname>",
+      streetAndNumber: "<streetAndNumber>",
+      zipCode: "zipCode",
+      city: "city",
+      lat: 0,
+      long: 0,
+      ok:true
+    },
+    {
+      id:5,
+      firstname: "<firstname>",
+      lastname: "<lastname>",
+      streetAndNumber: "<streetAndNumber>",
+      zipCode: "zipCode",
+      city: "city",
+      lat: 0,
+      long: 0,
+      ok:false
+    },
+    {
+      id:6,
+      firstname: "<firstname>",
+      lastname: "<lastname>",
+      streetAndNumber: "<streetAndNumber>",
+      zipCode: "zipCode",
+      city: "city",
+      lat: 0,
+      long: 0,
+      ok:false
+    },
+    {
+        id:6,
+        firstname: "<firstname>",
+        lastname: "<lastname>",
+        streetAndNumber: "<streetAndNumber>",
+        zipCode: "zipCode",
+        city: "city",
+        lat: 0,
+        long: 0,
+        ok:false
+      }
 ];
+export default data;
